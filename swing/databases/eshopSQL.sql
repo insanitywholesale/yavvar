874e521d15e3bbcefd3727126649b4dc6a394b29
lhz <pepitsizous@zoho.com>
2021-11-25 13:57:23 +0200

    fix bad formatting in eshopsql

--- swing/databases/eshopSQL.sql
+++ swing/databases/eshopSQL.sql
@@ -6,7 +6,6 @@
 --TODO: add users
 
 CREATE OR REPLACE FUNCTION create_tables() RETURNS void AS $$
-
     CREATE TABLE if not exists Categories (
         CategoryID SERIAL PRIMARY KEY NOT NULL,
         CategoryName VARCHAR NOT NULL,
@@ -64,7 +63,6 @@ CREATE OR REPLACE FUNCTION create_tables() RETURNS void AS $$
         ProductAdded TIMESTAMP NOT NULL DEFAULT NOW(),
         ProductUpdated TIMESTAMP NOT NULL DEFAULT NOW()
     );
-
 $$ LANGUAGE SQL;
 
 SELECT create_tables();
@@ -75,67 +73,7 @@ CREATE OR REPLACE FUNCTION assign_weight_class() RETURNS void AS $$
     WHERE ProductWeight >= 20.0;
 
     UPDATE Products
-    SET ProductWeightClass = 4CREATE OR REPLACE FUNCTION create_tables() RETURNS void AS $$
-
-    CREATE TABLE if not exists Categories (
-        CategoryID SERIAL PRIMARY KEY NOT NULL,
-        CategoryName VARCHAR NOT NULL,
-        CategoryDescription VARCHAR NOT NULL DEFAULT '',
-        CategoryAdded TIMESTAMP NOT NULL DEFAULT NOW(),
-        CategoryUpdated TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-
-    CREATE TABLE if not exists Manufacturers (
-        ManufacturerID SERIAL PRIMARY KEY NOT NULL,
-        ManufacturerName VARCHAR NOT NULL,
-        ManufacturerEmail VARCHAR NOT NULL,
-        ManufacturerAddress VARCHAR NOT NULL,
-        ManufacturerPhone VARCHAR NOT NULL,
-        ManufacturerAdded TIMESTAMP NOT NULL DEFAULT NOW(),
-        ManufacturerUpdated TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-
-    CREATE TABLE if not exists Orders (
-        OrderID SERIAL PRIMARY KEY NOT NULL,
-        OrderCost FLOAT NOT NULL,
-        OrderShippingCost FLOAT NOT NULL DEFAULT 5.0,
-        OrderTax FLOAT NOT NULL DEFAULT 24.0,
-        OrderTotalCost FLOAT NOT NULL,
-        OrderProcessed BOOLEAN NOT NULL DEFAULT FALSE,
-        OrderCity VARCHAR NOT NULL,
-        OrderShippingAddress VARCHAR NOT NULL,
-        OrderShippingAddress2 VARCHAR,
-        OrderZipcode VARCHAR NOT NULL,
-        OrderCountry VARCHAR NOT NULL,
-        OrderPhone VARCHAR NOT NULL,
-        OrderPhone2 VARCHAR,
-        OrderEmail VARCHAR NOT NULL,
-        OrderDate TIMESTAMP NOT NULL,
-        OrderHasShipped BOOLEAN NOT NULL DEFAULT FALSE, --TODO: if hasshipped, add tracking number
-        OrderHasArrived BOOLEAN NOT NULL DEFAULT FALSE,
-        OrderTrackingNumber VARCHAR,
-        OrderAdded TIMESTAMP NOT NULL DEFAULT NOW(),
-        OrderUpdated TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-
-    CREATE TABLE if not exists Products (
-        --ProductID INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-        ProductID SERIAL PRIMARY KEY NOT NULL,
-        ProductTitle VARCHAR NOT NULL,
-        ProductPrice FLOAT NOT NULL,
-        ProductCategoryID SERIAL REFERENCES Categories(CategoryID),
-        ProductManufacturerID SERIAL REFERENCES Manufacturers(ManufacturerID),
-        ProductDescription VARCHAR NOT NULL DEFAULT '',
-        ProductVersion FLOAT NOT NULL DEFAULT 1.0,
-        ProductWeight FLOAT,
-        ProductWeightClass SMALLINT,
-        ProductOnSale BOOLEAN NOT NULL DEFAULT FALSE,
-        ProductInStock BOOLEAN NOT NULL DEFAULT FALSE,
-        ProductAdded TIMESTAMP NOT NULL DEFAULT NOW(),
-        ProductUpdated TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-
-$$ LANGUAGE SQL;
+    SET ProductWeightClass = 4
     WHERE ProductWeight < 20.0;
 
     UPDATE Products
