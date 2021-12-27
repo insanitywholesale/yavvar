1c2f52b3159cb8f7c602bc2e4fdd2bbee1ab7859
lhz <pepitsizous@zoho.com>
2021-12-27 18:24:44 +0200

    upd8 db project

---
+++ swing/databases/DBUtils.java
@@ -0,0 +1,244 @@
+package swingdemo;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBUtils {
+
+    private static String driverClassName = "org.postgresql.Driver";
+    private static String url = "jdbc:postgresql://localhost:5432/tester";
+    private static String username = "tester";
+    private static String passwd = "Apasswd";
+    private static Connection dbConnection = null;
+    private static Statement statement = null;
+    private static int userID = -1;
+    private static String createTablesQuery = "";
+    private static String createAuditTablesQuery = "";
+    private static String createAuditTriggersQuery = "";
+    private static String createStoredProceduresQuery = "";
+
+    public static void initDB() {
+        loadSQL();
+        try {
+            //Load database driver
+            Class.forName(driverClassName);
+        } catch (ClassNotFoundException ex) {
+            //TODO: properly handle exception
+            System.out.println("driver load exception: " + ex);
+        }
+        try {
+            //Establish connection
+            dbConnection = DriverManager.getConnection(url, username, passwd);
+            //Make database connection globally available
+            statement = dbConnection.createStatement();
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println(ex);
+        }
+        try {
+            //Create base tables
+            statement.execute(createTablesQuery);
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("c_t exception: " + ex);
+        }
+        try {
+            //Create base tables
+            statement.executeQuery("SELECT create_tables();");
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("select c_t exception: " + ex);
+        }
+        try {
+            //Create audit tables
+            statement.execute(createAuditTablesQuery);
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("c_a_t exception: " + ex);
+        }
+        try {
+            //Create audit tables
+            statement.executeQuery("SELECT create_audit_tables();");
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("select c_a_t exception: " + ex);
+        }
+        try {
+            //Create audit triggers
+            statement.execute(createAuditTriggersQuery);
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("c_a_trig exception: " + ex);
+        }
+        try {
+            //Create stored procedures
+            statement.execute(createStoredProceduresQuery);
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("c_s_p exception: " + ex);
+        }
+        System.out.println("database set up successfully");
+    }
+
+    public static void loadSQL() {
+        try {
+            String sql = (BaseJFrame.class.getResource("eshopSQL.sql")).getPath();
+            Path fn = Path.of(sql);
+            String str = Files.readString(fn);
+        } catch (Exception ex) {
+            System.out.println("file thing fail 1: " + ex);
+        }
+        try {
+            String sql = (BaseJFrame.class.getResource("eshop-tables.sql")).getPath();
+            Path fn = Path.of(sql);
+            createTablesQuery = Files.readString(fn);
+        } catch (Exception ex) {
+            System.out.println("file thing fail 2: " + ex);
+        }
+        try {
+            String sql = (BaseJFrame.class.getResource("eshop-audittables.sql")).getPath();
+            Path fn = Path.of(sql);
+            createAuditTablesQuery = Files.readString(fn);
+        } catch (Exception ex) {
+            System.out.println("file thing fail 3: " + ex);
+        }
+        try {
+            String sql = (BaseJFrame.class.getResource("eshop-audittriggers.sql")).getPath();
+            Path fn = Path.of(sql);
+            createAuditTriggersQuery = Files.readString(fn);
+        } catch (Exception ex) {
+            System.out.println("file thing fail 4: " + ex);
+        }
+        try {
+            String sql = (BaseJFrame.class.getResource("eshop-operations.sql")).getPath();
+            Path fn = Path.of(sql);
+            createStoredProceduresQuery = Files.readString(fn);
+        } catch (Exception ex) {
+            System.out.println("file thing fail 5: " + ex);
+        }
+    }
+
+    private String addUser(String name, String email, String password, String fname, String lname) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT add_user_get_userid('" + name + "', '" + email + "', '" + password + "', '" + fname + "', '" + lname + "') as UID;");
+            while (rs.next()) {
+                String uid = rs.getString("UID");
+                System.out.println("a_u_g_u uid: " + uid);
+                return uid;
+            }
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("a_u_g_u exception: " + ex);
+        }
+        return "";
+    }
+
+    private String addAddress(String country, String city, String zip, String address, String phone) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT add_address_minimal('" + country + "', '" + city + "', '" + zip + "', '" + address + "', '" + phone + "') as AID;");
+            while (rs.next()) {
+                String aid = rs.getString("AID");
+                System.out.println("a_a_m aid: " + aid);
+                return aid;
+            }
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("a_a_m exception: " + ex);
+        }
+        return "";
+    }
+
+    private void addAddressToUser(String uid, String aid) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT set_user_address('" + uid + "', '" + aid + "');");
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("s_u_a exception: " + ex);
+        }
+    }
+
+    public static String userLogin(String nickname, String password) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT user_login('" + nickname + "', '" + password + "') AS UID;");
+            while (rs.next()) {
+                String uid = rs.getString("UID");
+                //TODO: handle null returned when password is wrong
+                System.out.println("u_l uid: " + uid);
+                return uid;
+            }
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("u_l exception: " + ex);
+        }
+        return "";
+    }
+
+    private void addCategory(String name, String desc) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT add_category_get_categoryid('" + name + "', '" + desc + "') AS CID;");
+            while (rs.next()) {
+                String cid = rs.getString("CID");
+                //TODO: handle null returned when password is wrong
+                System.out.println("a_c_g_c cid: " + cid);
+            }
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("a_c exception: " + ex);
+        }
+    }
+
+    private void addManufacturer(String name, String email, String address) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT add_manufacturer_with_address_get_manufacturerid('" + name + "', '" + email + "', '" + address + "') AS MID;");
+            while (rs.next()) {
+                String mid = rs.getString("MID");
+                //TODO: handle null returned when password is wrong
+                System.out.println("a_m_g_m mid: " + mid);
+            }
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("a_m_g_m exception: " + ex);
+        }
+    }
+
+    private void addProduct(String title, String price, String desc, String version, String weight) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT add_product_minimal('" + title + "', " + price + ", '" + desc + "', " + version + ", " + weight + ");");
+        } catch (SQLException ex) {
+            //TODO: properly handle exception
+            System.out.println("a_p_m exception: " + ex);
+        }
+    }
+}
+
+//EXAMPLES
+//
+/*
+String uid = addUser("anhel", "daddyinherently@cock.li", "joemomma", "An", "Hel");
+        if (uid.isEmpty()) {
+            System.out.println("problem adding user");
+        }
+        String aid = addAddress("greece", "salonika", "52525", "melenikou", "6900123456");
+        if (aid.isEmpty()) {
+            System.out.println("problem adding address");
+        }
+        String aid2 = addAddress("greece", "athina", "82345", "kapouekei", "210123456");
+        if (aid.isEmpty()) {
+            System.out.println("problem adding address 2");
+        }
+        addAddressToUser(uid, aid);
+        uid = userLogin("usr", "1234");
+        try {
+            userID = Integer.parseInt(uid);
+        } catch (NumberFormatException ex) {
+            System.out.println("uid str to int exception: " + ex);
+        }
+        addCategory("networking", "routers, switches and more");
+        addManufacturer("maker1", "pr@maker1.gr", "2");
+        addProduct("prodtitle", "50.0", "some prod", "3.2", "0.93");
+ */
---
+++ swing/databases/Login.java
@@ -0,0 +1,205 @@
+package swingdemo;
+
+public class Login extends javax.swing.JFrame {
+
+    public Login() {
+        initComponents();
+        DBUtils.initDB();
+        jPanel1.setFocusable(true);
+        jPasswordField1.setEchoChar((char) 0);
+        jPasswordField1.setText("Password");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jTextField1 = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPanel1MouseClicked(evt);
+            }
+        });
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jTextField1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jTextField1.setForeground(new java.awt.Color(204, 204, 204));
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField1.setText("Username");
+        jTextField1.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jTextField1FocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                jTextField1FocusLost(evt);
+            }
+        });
+        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 110, 250, 30));
+
+        jPasswordField1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jPasswordField1.setForeground(new java.awt.Color(204, 204, 204));
+        jPasswordField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jPasswordField1.setEchoChar('c');
+        jPasswordField1.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jPasswordField1FocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                jPasswordField1FocusLost(evt);
+            }
+        });
+        jPanel1.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 150, 250, 30));
+
+        jButton1.setBackground(new java.awt.Color(51, 102, 255));
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 102, 0));
+        jButton1.setText("Σύνδεση");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 230, 150, 40));
+
+        jButton2.setBackground(new java.awt.Color(255, 153, 0));
+        jButton2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(102, 255, 255));
+        jButton2.setText("Εγγραφή");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 280, 150, 40));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/swingdemo/643475.png"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField1FocusGained
+        if (jTextField1.getText().equals("Username")) {
+            jTextField1.setText("");
+            jTextField1.setForeground(new java.awt.Color(1, 1, 1));
+        }
+    }//GEN-LAST:event_jTextField1FocusGained
+
+    private void jTextField1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField1FocusLost
+        if (jTextField1.getText().equals("")) {
+            jTextField1.setText("Username");
+            jTextField1.setForeground(new java.awt.Color(204, 204, 204));
+        }
+    }//GEN-LAST:event_jTextField1FocusLost
+
+    private void jPasswordField1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jPasswordField1FocusGained
+        if (jPasswordField1.getText().equals("Password")) {
+            jPasswordField1.setText("");
+            jPasswordField1.setEchoChar('*');
+            jPasswordField1.setForeground(new java.awt.Color(1, 1, 1));
+        }
+    }//GEN-LAST:event_jPasswordField1FocusGained
+
+    private void jPasswordField1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jPasswordField1FocusLost
+        if (jPasswordField1.getText().equals("")) {
+            jPasswordField1.setEchoChar((char) 0);
+            jPasswordField1.setText("Password");
+            jPasswordField1.setForeground(new java.awt.Color(204, 204, 204));
+        }
+    }//GEN-LAST:event_jPasswordField1FocusLost
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String loginResult = DBUtils.userLogin(jTextField1.getText(), jPasswordField1.getText());
+        if (loginResult != "" && loginResult != null) {
+            Menu Menu = new Menu();
+            Menu.setSize(700, 450);
+            Menu.setVisible(true);
+            this.setVisible(false);
+            this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+            this.dispose();
+            System.out.println("logged in with uid: " + loginResult);
+        } else if (loginResult == null) {
+            System.out.println("wrong password");
+        } else {
+            System.out.println("empty result");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        Register Register = new Register();
+        Register.setSize(700, 450);
+        Register.setVisible(true);
+        this.setVisible(false);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jPanel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseClicked
+        jTextField1.setFocusable(false);
+        jPasswordField1.setFocusable(false);
+        jPanel1.setFocusable(true);
+        jTextField1.setFocusable(true);
+        jPasswordField1.setFocusable(true);
+    }//GEN-LAST:event_jPanel1MouseClicked
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
